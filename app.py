8c1c23dcf0af6fa9936ea4f5e3b7797e441e5a39
Veer Sharma <73035101+veer-sharma@users.noreply.github.com>
2023-02-04 10:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,4 +171,5 @@ def add():
 if __name__ == '__main__':
     #app.run(debug=True)
     from waitress import serve
-    serve(app, host="dpg-cfdq2782i3mmlo3nicq0-a", port=5432)
+    serve(app, host="0.0.0.0", port=8080)
+
